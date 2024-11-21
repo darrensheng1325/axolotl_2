029cb148de052fa0e97906fe0e4a124b9c9e8c02
darrensheng1325 <darrensheng1325@gmail.com>
2024-11-20 19:52:02 -0800

    added host page

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,54 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: {
+        game: './src/index.ts',
+        server_worker: './src/server_host.ts'
+    },
+    target: 'webworker',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
+            }
         ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        fallback: {
+            "fs": false,
+            "path": require.resolve("path-browserify"),
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "buffer": require.resolve("buffer/"),
+            "util": require.resolve("util/"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "zlib": require.resolve("browserify-zlib"),
+            "net": false,
+            "tls": false,
+            "child_process": false
+        }
     },
     plugins: [
+        new NodePolyfillPlugin(),
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+            process: 'process/browser'
+        }),
         new CopyWebpackPlugin({
             patterns: [
                 { from: 'src/index.html', to: 'index.html' },
+                { from: 'host.html', to: 'host.html' },
                 { from: 'src/styles.css', to: 'styles.css' },
                 { from: 'assets', to: 'assets' }
             ],
